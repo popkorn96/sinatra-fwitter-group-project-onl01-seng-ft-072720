d008d750000565433604fc2fcd1d52e86794954b
popkorn96 <mpaloma2014@gmail.com>
2020-09-12 17:07:15 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,27 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
   end
-
+  get "/" do
+    erb :"/home/index"
+  end
+  get "/signup" do
+      erb :"/home/signup"
+      redirect "/users/tweets"
+  end
+  post "/signup" do 
+    user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    if user.save
+      # session[:user_id] = @user.id
+      redirect to "/users/tweets"
+    else params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to "/home/signup"
+    end
+  end
+  get "/login" do 
+    erb :"/home/login"
+    redirect to "/users/tweets"
+  end
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+  validates_presence_of :username, :email, :password
+  def self.current_user(session)
+    User.find(session[:user_id])
+  end
+  def self.is_logged_in?(session)
+    session[:user_id] ? true : false    
+  end
 end
